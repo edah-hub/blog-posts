5deffbf2c2fa03940ebd7f6d71ad07d4ecebe5f7
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-05-16 02:30:17 +0300

    adds registration forms

---
+++ app/auth/forms.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
+from app.models import User
+
+class RegistrationForm(FlaskForm):
+  username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+  email = StringField('Email', validators=[DataRequired(), Email()])
+  password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+  confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+  submit = SubmitField('Sign Up')
+  
+  def validate_username(self, username):
+    user = User.query.filter_by(username = username.data).first()
+    if user: 
+      raise ValidationError('That username is already taken. Please choose another one')
+  
+  def validate_email(self, email):
+    user = User.query.filter_by(email = email.data).first()
+    if user: 
+      raise ValidationError('That email is already taken. Please choose another one')
+
+class LoginForm(FlaskForm):
+  email = StringField('Email', validators=[DataRequired(), Email()])
+  password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+  remember = BooleanField('Remember Me')
+  submit = SubmitField('Login')
